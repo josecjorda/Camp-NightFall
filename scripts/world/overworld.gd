2ed67d733f7ce55fed7c788dc67993358f8219af
josecjorda <96598151+josecjorda@users.noreply.github.com>
2024-04-10 17:42:27 -0500

    MapBackground, Archery range, and Bathroom added

--- scripts/world/overworld.gd
+++ scripts/world/overworld.gd
@@ -2,18 +2,48 @@ extends Node2D
 
 ## All enterable buildings should be area2d nodes under the buildings node. 
 ## They should all have a OutsideView 2dnode that shows the outside view prior to entering 
-## to adjust visibility.
+## to adjust visibility. Set OutsideView oredering z index to 1 so items/npcs don't appear on top
+## of the outside layer. Player should appear on top of outside view so keep z index as 2.
+## When adding buildings, turn off OutsideView visibility for convenience when
+## placing npcs/items on map.[br]
+##
+## Make sure to set proper collison layers for nodes other wise buildings might think node is player.
 
 func _ready():
 	## Connects signals to buildings.
+	$MapBackground.get_node("map").play("default")
+	$other_locations/Archery.get_node("AnimatedSprite2D").play("default")
 	for building in $buildings.get_children():
+		building.get_node("OutsideView").visible = true
 		building.body_entered.connect(building_entered.bind(building))
 		building.body_exited.connect(building_exited.bind(building))
 
-
+## Makes interiror of building visible
 func building_entered(player, building):
 	building.get_node("OutsideView").visible = false
+	dim_except_for(building)
 
-
+## Shows outside view of building
 func building_exited(player, building):
 	building.get_node("OutsideView").visible = true
+	undim_all()
+
+
+## Essentially makes everything outside the building one's currently in black.
+## To make outside of building dimmed out, modulate map, other_locations, and buildings except for entered one
+func dim_except_for(building):
+	$other_locations.visible = false
+	$MapBackground.modulate = "black"
+	var buildings = $buildings.get_children()
+	buildings.erase(building)
+	for build in buildings:
+		build.get_node("OutsideView").modulate = "black"
+
+
+## Undims world and buildings/locations
+func undim_all():
+	$other_locations.visible = true
+	$MapBackground.modulate = "white"
+	var buildings = $buildings.get_children()
+	for build in buildings:
+		build.get_node("OutsideView").modulate = "white"
